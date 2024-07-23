fc8aae6185f489e28dbe2ed59a327bd59a03e07d
Артём Щукин <artemschukin@MacBook-Pro-Artem.local>
2024-07-23 22:35:13 +0300

    Fix isort wrong.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,10 @@
+from api.views import TaskView
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from api import views
 
 router = routers.DefaultRouter()
-router.register('tasks', views.TaskView, 'task')
+router.register('tasks', TaskView, 'task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
